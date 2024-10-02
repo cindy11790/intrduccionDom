1328b36046db5fae2ce165c1a4f4f874279d79f6
cindy11790 <cindyestefaniazuluagacastano@gmail.com>
2024-10-02 16:10:06 -0500

    intrduccion al dom

---
+++ view/public/js/scripts.js
@@ -0,0 +1,23 @@
+/*selectores
+getElement 
+    getElementById   /*elemento por id que  primero wue encuentre
+    getElementClassNmae /*elemento por clase, uno o multiples elementos, es decir muestra un arreglo
+    getElementByTagName /*elemento por nombre de etiqueta, siempre me va mostrar un arreglo
+
+querySelector
+    querySelector    ---permite seleccionar por id, por clase y por etiqueta, si debo pasarle el elemento
+    querySelectorAll ----permite seleccionar por id id, por clase y por  etiqueta
+
+
+
+
+
+*/
+/*console.log(document.getElementById("usuario").textContent = 'un nuevo usuario')*/             /* document es todo mi html*/ /*getelementbyclass, los parentesis son para poner el id*/
+                                                                                               /*textcontent es para agregar o poner otro nombre*/
+
+
+
+
+
+
